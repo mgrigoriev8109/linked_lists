becaaff42fab20fef2abbe509150d3464e7163f8
Mikhail <mikhailgrigoriev92@gmail.com>
2021-09-22 15:18:02 -0400

    Add append and prepend functionality

--- create_link_list.rb
+++ create_link_list.rb
@@ -4,8 +4,7 @@ require_relative 'linked_list'
 linked_list = LinkedList.new
 first_node = linked_list.append(24)
 second_node = linked_list.append(27)
-
-p "node #{first_node.value} is pointing to #{first_node.next_node}"
 first_node.next_node = second_node
+
 p "node #{first_node.value} is pointing to #{first_node.next_node}"
 p "node #{second_node.value} is pointing to #{second_node.next_node}"
--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,27 @@
 class LinkedList
-@@node_size = 0
+@node_size = 0
+@head = false
 
   def append(value = nil, next_node = nil)
-    Node.new(value, next_node)
+    if @head == false 
+      @head = Node.new(value, next_node)
+    else
+      Node.new(value, next_node)
+    end
+    @node_size += 1
   end
+
+  def prepend(value = nil, next_node = nil)
+    @head = Node.new(value, next_node)
+    @node_size += 1
+  end
+
 end
+
+Time to write in pseudocode
+
+I need to created a linked list in Ruby using two classes
+This class, LinkedList will represent the full list
+It creates new instances of the Node class every time append is called
+These new instances will always point to nil when appended
+
